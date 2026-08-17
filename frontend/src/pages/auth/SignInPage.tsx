261d0bce19dfae770b5d7b0f0061f384204a3158
cuycorp <cuycorp@gmail.com>
2026-08-17 15:56:04 +0200

    feat(pages) Set basic SignIn and SignUp pages

--- frontend/src/pages/auth/SignInPage.tsx
+++ frontend/src/pages/auth/SignInPage.tsx
@@ -1,4 +1,42 @@
+import { useState, FormEvent } from "react";
+
 export function SignInPage()
 {
-    return <h1>SignInPage</h1>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        alert("submitted with email: " + email + " and password: " + password);
+    }
+    return (
+    <form onSubmit={handleSubmit}>
+        <h1>Sign In</h1>
+            <div>
+                <label>
+                    Email 
+                    <input 
+                    type ="email"
+                    placeholder="...@..."
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+            </div>
+            
+            <div>
+                <label>
+                    Password 
+                    <input 
+                        type="password"
+                        placeholder="**********"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+            </div>
+            <button type="submit">Sign in</button>
+    </form>
+    
+    );
 }
--- frontend/src/pages/auth/SignUpPage.tsx
+++ frontend/src/pages/auth/SignUpPage.tsx
@@ -3,15 +3,41 @@ import {useState, FormEvent} from "react";
 export function SignUpPage()
 {
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        alert("submitted with email: " + email + " and password: " + password);
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Sign up</h1>
-            <input 
-            type ="email"
-            placeholder="Email..."
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
+            <div>
+                <label>
+                    Email 
+                    <input 
+                    type ="email"
+                    placeholder="...@..."
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+            </div>
+            
+            <div>
+                <label>
+                    Password 
+                    <input 
+                        type="password"
+                        placeholder="**********"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+            </div>
+            <button type="submit">Create account</button>
         </form>
     );
 }
+
+// todo: Validate password(+min len) and email are entered: is it frontend or backend job?
