8a79a86142c90736cbaebd1d989614100b9c95f0
cuycorp <cuycorp@gmail.com>
2026-08-25 00:04:45 +0200

    Added new version authApi

--- frontend/src/api/L_auth.api.ts
+++ frontend/src/api/L_auth.api.ts
@@ -1,9 +1,8 @@
 //TODO: erase or remove duplicate auth.api.ts
-
-
 import { mockAccounts, mockSession, mockUsers,} from "../mocks/workspace.mock";
 import type { SignInInput, SignUpInput, User, } from "../types/workspace.types";
 import { cloneMockValue, MockApiError, waitForMockApi, } from "./mock-api.utils";
+
 
 export async function getCurrentUser(): Promise<User | null> {
   await waitForMockApi();
